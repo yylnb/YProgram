cd2f6e2b129d4b95975098d2b235902d1af748ea
Leon Yeung <yyl_yyds@qq.com>
2025-12-17 23:54:09 +0800

    Added library insert script of python.

---
+++ Libraries/Python/Beginner/Insert2.sql
@@ -0,0 +1,15 @@
+INSERT INTO study_app.library
+(lang, lb_id, difficulty, page, title, tags, summary, content, eg_in, eg_out)
+VALUES
+(
+  'python',
+  1,
+  'beginner',
+  18,
+  '变量的概念',
+  '[\"变量\", \"基础语法\"]',
+  '了解什么是变量以及变量的基本作用。',
+  '变量用于在程序中存储数据。可以通过赋值语句将数据保存到变量名中，之后可以反复使用。',
+  '[\"x = 10\\nprint(x)\"]',
+  '[\"10\"]'
+)
